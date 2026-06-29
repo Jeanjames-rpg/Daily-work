452f04cb6f34e30d0c3ba05284e4d7a5d9979071
Jean James <jeanjamey004@gmail.com>
2026-06-29 10:33:50 +0530

    added tailwind

--- Course_provider/frontend/src/App.js
+++ Course_provider/frontend/src/App.js
@@ -57,6 +57,11 @@ function App(){
     
     <Footer/>
     </BrowserRouter>
+    // <div className='min-h-screen bg-orange-100 flex items-center justify-center'>
+    //   <h1 className='text-5xl font-bold text-orange-600' >
+    //     Tailwind is working!
+    //   </h1>
+    // </div>
   );
 
 
