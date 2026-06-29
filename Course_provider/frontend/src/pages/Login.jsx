a35613f3ee6566b1871cd9b9dc9057dacc68b2f3
Jean James <jeanjamey004@gmail.com>
2026-06-29 14:34:52 +0530

    header&login tailwind

--- Course_provider/frontend/src/pages/Login.jsx
+++ Course_provider/frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import api from "../services/api";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import styles from "../styles/Register_login.module.css"
+
 import logo from "../assets/logo2.PNG"
 
 function Login() {
@@ -44,32 +44,36 @@ function Login() {
 
 
         return(
-         <div className={styles.container}>
-            <img src={logo} alt="Logo" className={styles.logo} />
-            
-            <form onSubmit={handleLogin} className={styles.form} >
+         <div className="min-h-screen flex items-center justify-center bg-slate-100 px-4">
+            <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md">
 
-                <input placeholder="Username" className={styles.input} onChange={(e) => setUsername(e.target.value)}/>
+            <div className="flex justify-center mb-6">
+            <img src={logo} alt="Logo" className="h-32 w-32 object-contain mx-auto mb-6" />
+            </div>
+            <form onSubmit={handleLogin} className="space-y-5" >
 
-                <input type="password"  placeholder="Password" className={styles.input} onChange={(e) => setPassword(e.target.value)} />   
+                <input placeholder="Username" className="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-indigo-500" onChange={(e) => setUsername(e.target.value)}/>
 
-                <button  className={styles.button} type="submit">
+                <input type="password"  placeholder="Password" className='w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-indigo-500' onChange={(e) => setPassword(e.target.value)} />   
+
+                <button  className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700 transition duration-300 font-semibold" type="submit">
                     Login
                 </button>
 
-                <p className={styles.links}>
-                    Return to our Homepage <Link to= "/">Home</Link>
-                </p>
-
-                <p className={styles.links}>
-                    Dont have an account?
-                    <Link to='/register'>Register</Link>
-                </p>
-
              </form>
              
-             
+            <div className="mt-6 text-center space-y-2">
+             <p className='text-gray-600'>
+                    Return to our {' '} <Link to= "/" className="text-indigo-600 hover:underline font-medium">Homepage</Link>
+                </p>
 
+                <p className="text-gray-600">
+                    Dont have an account?{' '}
+                    <Link to='/register' className="text-indigo-600 hover:underline font-medium">Register</Link>
+                </p>
+            </div> 
+            
+            </div>   
          </div>
         );
 
