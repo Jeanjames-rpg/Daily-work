53278842165fe352592a69b1b51c59344cba71f4
Jean James <jeanjamey004@gmail.com>
2026-08-12 00:41:22 +0530

    auto login on register

--- Course_provider/backend/accounts/views.py
+++ Course_provider/backend/accounts/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .serializers import UserUpdateSerializer
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 
@@ -14,6 +14,38 @@ class RegisterView(generics.CreateAPIView):
 
     serializer_class = RegisterSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
+
+        response = Response({
+            "message": "Registeration successful"
+        })
+
+        response.set_cookie(
+            key="access",
+            value=str(access),
+            httponly=True,
+            secure=False,
+            samesite=None,
+            max_age=60*60
+        )
+
+        response.set_cookie(
+            key="refresh",
+            value=str(refresh),
+            httponly=True,
+            secure=False,
+            samesite=None,
+            max_age=7 * 24 * 60 * 60
+        )
+
+        return response
 class MeView(APIView):
 
     permission_classes = [IsAuthenticated]
